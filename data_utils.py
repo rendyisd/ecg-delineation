af5998390f6424074691627581827329ea9bb588
Rendy I <risdwanta35@gmail.com>
2023-08-06 22:36:39 +0700

    Initial commit

---
+++ data_utils.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import pywt
+from scipy import stats
+
+import wfdb
+
+# Helper
+def grouped(itr, n=3):
+    itr = iter(itr)
+    end = object()
+    while True:
+        vals = tuple(next(itr, end) for _ in range(n))
+        if vals[-1] is end:
+            return
+        yield vals
+
+'''
+signal: an array (self-explanatory)
+wavelet: either wavelet name string or wavelet object
+level: an integer, decomposition level
+'''
+def universal_threshold(coeffs):
+    # Flatten the detail coefficients (excluding the approximation coefficients)
+    detail_coeffs = np.concatenate(coeffs[1:])
+
+    # Compute the noise variance estimate using Median Absolute Deviation (MAD)
+    noise_var = (stats.median_abs_deviation(detail_coeffs) / 0.6745) ** 2
+
+    # Calculate the universal threshold based on the SURE criterion
+    threshold = np.sqrt(2 * noise_var * np.log(len(detail_coeffs)))
+
+    return threshold
+
+# TODO:find the reference for SURE threshold
+def denoise_dwt(signal, wavelet, level):
+    coeffs = pywt.wavedec(signal, wavelet, level=level)
+    '''
+    As per the pywt.wavedec docs:
+    coeffs[0] contains approximation coeffs and the rest are detail coeffs
+    '''
+    # threshold = universal_threshold(coeffs)
+
+    for i in range(1, len(coeffs)):
+        threshold = stats.median_abs_deviation(coeffs[i])
+        coeffs[i] = pywt.threshold(coeffs[i], threshold, mode='hard')
+    
+    denoised_signal = pywt.waverec(coeffs, wavelet)
+
+    return denoised_signal
+
+def calculate_snr(original_signal, denoised_signal):
+    if len(original_signal) != len(denoised_signal):
+        raise ValueError("Original and denoised signal must have the same length")
+    
+    original_signal = np.asarray(original_signal)
+    denoised_signal = np.asarray(denoised_signal)
+
+    original_sq = np.sum(original_signal ** 2)
+
+    noise = original_signal - denoised_signal
+    noise_sq = np.sum(noise ** 2)
+
+    # result in decibels
+    snr = 10 * np.log10(original_sq / noise_sq)
+
+    return snr
+
+# Below function is from The Optimal Selection of Mother Function ... paper
+# def calculate_snr(original_signal, denoised_signal):
+#     original_signal = np.asarray(original_signal)
+#     denoised_signal = np.asarray(denoised_signal)
+
+#     numerator = np.sum(original_signal ** 2)
+#     denominator = np.sum(np.absolute(original_signal - denoised_signal))
+
+#     snr = 10 * np.log10(numerator / denominator)
+
+#     return snr
---
+++ ecg_signal.py
@@ -0,0 +1,212 @@
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+import pywt
+from scipy import stats
+
+import wfdb
+
+import data_utils
+
+CURR_DIR = os.getcwd()
+DATA_DIR = os.path.join(CURR_DIR, 'data/ludb')
+
+SEGMENTS_STR = {
+    0: 'Pon-Poff',
+    1: 'Poff-QRSon',
+    2: 'QRSon-Rpeak',
+    3: 'Rpeak-QRSoff',
+    4: 'QRSoff-Ton',
+    5: 'Ton-Toff',
+    6: 'Toff-Pon2'
+}
+# for better readability
+SEGMENTS_NUM = {
+    'Pon-Poff': 0,
+    'Poff-QRSon': 1,
+    'QRSon-Rpeak': 2,
+    'Rpeak-QRSoff': 3,
+    'QRSoff-Ton': 4,
+    'Ton-Toff': 5,
+    'Toff-Pon2': 6
+}
+
+
+SEGMENT_TO_COLOR = {
+    0: 'red',
+    1: 'darkorange',
+    2: 'yellow',
+    3: 'green',
+    4: 'blue',
+    5: 'darkcyan',
+    6: 'purple'
+}
+
+# LEADS = ['atr_avf', 'atr_avl', 'atr_avr', 'atr_i', 'atr_ii', 'atr_iii', 'atr_v1', 'atr_v2', 'atr_v3', 'atr_v4', 'atr_v5', 'atr_v6']
+LEADS = ['atr_i', 'atr_ii', 'atr_iii']
+
+class ECGSignal:
+    def __init__(self, signal, samples, symbols):
+        self.signal = signal
+        self.samples = samples
+        self.symbols = symbols
+
+        self.segment_map, self.segment_start_end = self.segmentate()
+
+    @staticmethod
+    def load_ecg_signal(record_number, lead):
+        record_dir = os.path.join(DATA_DIR, str(record_number))
+
+        record = wfdb.rdrecord(record_dir)
+        ann = wfdb.rdann(record_dir, lead)
+
+        signal = record.p_signal[:, LEADS.index(lead)]
+        samples = ann.sample
+        symbols = ann.symbol
+
+        return ECGSignal(signal, samples, symbols)
+    
+    @staticmethod
+    def plot_signal_segments(signal, segment_map):
+        _, ax = plt.subplots(figsize=(28, 3))
+
+        ax.plot(signal)
+
+        segments = []
+        legend_patches = []
+
+        ptr_start = 0
+        ptr_end = 0
+
+        # 0 1 2 3 4 5 6
+        curr_seg = segment_map[0]
+        segments.append(curr_seg)
+
+        for idx, seg in enumerate(segment_map):
+            if seg != curr_seg:    
+                ptr_end = idx - 1
+
+                # the if here is TEMP
+                if(curr_seg != -1):
+                    color = SEGMENT_TO_COLOR[curr_seg]
+                    ax.axvspan(ptr_start, ptr_end, color=color, alpha=0.4)
+
+                    if curr_seg not in segments:
+                        segments.append(curr_seg)
+
+                curr_seg = seg
+                ptr_start = idx - 1
+            
+            # at the end of segment_map
+            elif idx == len(segment_map) - 1:
+                ptr_end = idx
+
+                if(curr_seg != -1):
+
+                    color = SEGMENT_TO_COLOR[curr_seg]
+                    ax.axvspan(ptr_start, ptr_end, color=color, alpha=0.4)
+
+                    if curr_seg not in segments:
+                        segments.append(seg)
+        
+        for seg in sorted(segments):
+            patch = patches.Patch(color=SEGMENT_TO_COLOR[seg], label=SEGMENTS_STR[seg])
+            legend_patches.append(patch)
+
+        ax.legend(handles=legend_patches, loc='upper right')
+
+    def plot_signal_samples(self):
+        _, ax = plt.subplots(figsize=(28, 3))
+
+        ax.plot(self.signal)
+        ax.scatter(self.samples, self.signal[self.samples], c='red')
+    
+    def plot_segments(self):
+        _, ax = plt.subplots(figsize=(28, 3))
+
+        ax.plot(self.signal)
+
+        segments = []
+        legend_patches = []
+        
+        for _, points in self.segment_start_end:
+            start, end = points
+            color = SEGMENT_TO_COLOR[self.segment_map[start]]
+            ax.axvspan(start, end, color=color, alpha=0.4)
+
+            if self.segment_map[start] not in segments:
+                segments.append(self.segment_map[start])
+        
+        # make patches for existing segments
+        for seg in sorted(segments):
+            patch = patches.Patch(color=SEGMENT_TO_COLOR[seg], label=SEGMENTS_STR[seg])
+            legend_patches.append(patch)
+
+        ax.legend(handles=legend_patches, loc='upper right')
+        
+    def segmentate(self):
+        segment_map = np.full(len(self.signal), -1)
+        segment_start_end = []
+
+        prev_symbol = None
+        for start, peak, end in data_utils.grouped(self.samples):
+            symbol_idx = np.where(self.samples == peak)[0][0]
+
+            curr_symbol = self.symbols[symbol_idx]
+
+            if curr_symbol == 'p':
+                if prev_symbol is not None and prev_symbol == 't':
+                    segment_map[prev_end:start] = SEGMENTS_NUM['Toff-Pon2']
+                    segment_start_end.append((SEGMENTS_NUM['Toff-Pon2'], (prev_end, start)))
+
+                segment_map[start:end] = SEGMENTS_NUM['Pon-Poff']
+                segment_start_end.append((SEGMENTS_NUM['Pon-Poff'], (start, end)))
+                
+
+            elif curr_symbol == 'N':
+                if prev_symbol == 'p':
+                    segment_map[prev_end:start] = SEGMENTS_NUM['Poff-QRSon']
+                    segment_start_end.append((SEGMENTS_NUM['Poff-QRSon'], (prev_end, start)))
+
+                segment_map[start:peak] = SEGMENTS_NUM['QRSon-Rpeak']
+                segment_start_end.append((SEGMENTS_NUM['QRSon-Rpeak'], (start, peak)))
+
+                segment_map[peak:end] = SEGMENTS_NUM['Rpeak-QRSoff']
+                segment_start_end.append((SEGMENTS_NUM['Rpeak-QRSoff'], (peak, end)))
+
+            elif curr_symbol == 't':
+                if prev_symbol == 'N':
+                    segment_map[prev_end:start] = SEGMENTS_NUM['QRSoff-Ton']
+                    segment_start_end.append((SEGMENTS_NUM['QRSoff-Ton'], (prev_end, start)))
+
+                segment_map[start:end] = SEGMENTS_NUM['Ton-Toff']
+                segment_start_end.append((SEGMENTS_NUM['Ton-Toff'], (start, end)))
+            
+            prev_symbol = curr_symbol
+            prev_end = end
+        
+        return segment_map, segment_start_end
+
+    # return signal and segment_map
+    def cut_per_beat(self):
+        # beats list contains tuple (signal, segment_map) of each beat
+        beats = []
+        prev_p_start = None
+
+        for seg, points in self.segment_start_end:
+            start, _ = points
+
+            if(seg == SEGMENTS_NUM['Pon-Poff']):
+                if prev_p_start is not None:
+                    signal = self.signal[prev_p_start:start]
+                    segment_map = self.segment_map[prev_p_start:start] 
+
+                    beats.append((signal, segment_map))                
+
+                prev_p_start = start
+        
+        return beats
+            
